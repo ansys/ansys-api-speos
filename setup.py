1ed35a67b2bfced9e13812bcea5f59b62f63b4f0
Etienne Arnal <95644345+etiennearnal@users.noreply.github.com>
2023-09-25 09:52:47 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# (c) 2024 ANSYS, Inc. Unauthorized use, distribution, or duplication is prohibited. ANSYS Confidential Information
+# (c) 2024 ANSYS, Inc. Unauthorized use, distribution, or duplication is prohibited.
 """Installation file for the ansys-api-speos package"""
 
 import os
