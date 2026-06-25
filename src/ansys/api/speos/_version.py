e0938c3044301faa8efad8d69f8f1fd810f27ef9
Elodie Chamblas <echambla@synopsys.com>
2026-06-25 14:35:55 +0200

    ver: bump version 0.16.3

--- src/ansys/api/speos/_version.py
+++ src/ansys/api/speos/_version.py
@@ -6,7 +6,7 @@ version_info = 0, 1, 'dev0'
 """
 
 # major, minor, patch
-version_info = 0, 16, "2"
+version_info = 0, 16, "3"
 
 # Nice string for the version
 __version__ = ".".join(map(str, version_info))
